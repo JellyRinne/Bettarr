eec19f0ab876c1e04596df55ee91e143eec2ae2c
JellyRinne <jelly.rinne@icloud.com>
2024-12-04 12:33:23 -0800

    ?

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 # Install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    brew update
+fi
 
 # Install redis and configure it as a service (auto-start on boot)
 brew install redis
@@ -10,11 +16,13 @@ brew install python
 python3 -m venv ./venv
 source ./venv/bin/activate
 
-# Install pip
-python3 -m ensurepip --upgrade
+# Install and upgrade pip
+python3 -m ensurepip --upgrade pip
+python3 -m pip install --upgrade pip
 
 #Install python dependencies
 python3 -m pip install flask
 python3 -m pip install flask_bootstrap
 python3 -m pip install turbo_flask
 python3 -m pip install gunicorn
+python3 -m pip install "redis[hiredis]"
