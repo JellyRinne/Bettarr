05495115e8b428b567484c74f82ed4e2cedfa831
JellyRinne <jelly.rinne@icloud.com>
2024-12-04 12:08:47 -0800

    env setup and launch/config script

---
+++ app.py
@@ -0,0 +1,8 @@
+import datetime
+import logging
+import time
+import threading
+
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from turbo_flask import Turbo
--- setup.sh
+++ setup.sh
@@ -1,4 +1,19 @@
+# Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install redis and configure it as a service (auto-start on boot)
 brew install redis
 brew services start redis
+
+# Install python and initialize a virtual environment
 brew install python
+python3 -m venv ./venv
+source ./venv/bin/activate
+
+# Install pip
 python3 -m ensurepip --upgrade
+
+#Install python dependencies
+python3 -m pip install flask
+python3 -m pip install flask_bootstrap
+python3 -m pip install turbo_flask
