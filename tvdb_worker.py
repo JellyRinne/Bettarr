3b6a1286fe761e46dfef99bd7ef49659456e9db4
JellyRinne <jelly.rinne@icloud.com>
2024-12-04 16:46:45 -0800

    movies updater completed

--- tvdb_worker.py
+++ tvdb_worker.py
@@ -31,12 +31,45 @@ def getMoviesInitialSync():
         else:
             break
 
-    #save changes to disc
-    rc2.bgsave()
-
     #log end time of initial sync
     rc1.set('allMovieInitialSyncEnd',str(datetime.datetime.now()))
-    rc1.bgsave()
+
+def getUpdateActionUpdateMovies():
+    #connect to meta db
+    rc1 = redis.Redis(host='localhost', port=6379, db=constants.metadb, charset="utf-8", 
+                      decode_responses=True)
+
+    #connect to movies db
+    rc2 = redis.Redis(host='localhost', port=6379, db=constants.moviesdb, charset="utf-8", 
+                      decode_responses=True)
+    tvdb = tvdb_v4_official.TVDB(constants.tvdb_apikey)
+
+    #log start time of sync
+    rc1.set('allMovieLastUpdateStart',str(datetime.datetime.now()))
+
+    #get end of initial movie sync as epoch time
+    dt = datetime.datetime.strptime(rc1.get('allMovieInitialSyncEnd'),constants.date_format)
+    lastUpdateEpoch = int(dt.timestamp())
+    
+    #retrieve movie updates since that sync time
+    page = 0
+
+    while True:
+        movies = tvdb.get_updates(action='update', since=lastUpdateEpoch, page=page)
+        if (movies != []):
+            for movie in movies:
+                rc2.set(movie['id'],str(movie))
+            page += 1
+            movies = []
+        else:
+            break
+
+    #log end time of sync
+    rc1.set('allMovieLastUpdateStart',str(datetime.datetime.now()))
+    
+#getMoviesInitialSync()
+getUpdateActionUpdateMovies()
+
     
 
 
