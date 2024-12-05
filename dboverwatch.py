9e3d0b5348a58f94f15184ae829a691ee63bc28e
JellyRinne <jelly.rinne@icloud.com>
2024-12-04 17:03:55 -0800

    little nicer

--- dboverwatch.py
+++ dboverwatch.py
@@ -1,8 +1,8 @@
 import datetime
 import logging
 import time
-import threading
 import schedule
+import redis
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename='dboverwatch.log', encoding='utf-8', level=logging.DEBUG)
@@ -18,12 +18,22 @@ def setup():
 def every15Minutes():
     tvdb_workers.getUpdateActionUpdateMovies
 
+# Schedule queues
 schedule.every(15).minutes.do(every15Minutes)
 
 def runtime():
+    #connect to meta db
+    rc1 = redis.Redis(host='localhost', port=6379, db=constants.metadb, charset="utf-8", 
+                      decode_responses=True)
+    
+    # Check for a finished Initial All Movies Sync. If we don't have one, run setup syncs
+    if not rc1.get('allMovieInitialSyncEnd'):
+        setup()
+
+    # Start runtime of scheduled tasks
     while True:
         schedule.run_pending()
         time.sleep(1)
 
-runtime()
+#runtime()
 
